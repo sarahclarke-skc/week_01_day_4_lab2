82b099ab070c6678110ae0e8130aace0d5a181a2
sarahclarke-skc <sarahkathrynclarke@gmail.com>
2021-07-22 15:49:07 +0100

    completed first 3 tasks

---
+++ starting_point/src/friends.py
@@ -0,0 +1,11 @@
+def get_name(person):
+    return person["name"]
+
+def get_favourite_tv_show(person):
+    return person["favourites"]["tv_show"]
+
+def likes_to_eat(person, food):
+    if food in person["favourites"]["snacks"]:
+        return True
+    else: 
+        return False
